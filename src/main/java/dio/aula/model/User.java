6a616d25299f51f6a7f9c131213131914a67e466
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-06-28 20:09:37 -0300

    Create User.java

---
+++ src/main/java/dio/aula/model/User.java
@@ -0,0 +1,43 @@
+package dio.aula.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+    private String name;
+    private String username;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
